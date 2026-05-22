5ab28bcbe3262842e0aa890698c711abd1704e39
Diogo Andrade <da7822000@gmail.com>
2026-05-22 23:54:59 +0200

    init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+   css: [
+    '~/assets/css/global.css'
+  ],
+  app: {
+    head: {
+      
+      title: 'Porte-Folio Diogo Andrade',
+      meta: [
+        { name: 'description', content: 'Porte-Folio de Diogo Andrade' },
+      ],
+      htmlAttrs: { lang: 'fr-FR' },
+    }
+  }
 })
