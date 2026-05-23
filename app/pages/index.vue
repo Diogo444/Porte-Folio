50497d418c6d5dc2091972efbd58dbffe338d82e
Diogo Andrade <da7822000@gmail.com>
2026-05-23 12:11:39 +0200

    init all files

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,9 @@
 <template>
-    <div class="home">
-        <h1>Welcome to the Home Page</h1>
-        <p>This is the main landing page of the application.</p>
-    </div>
+    <main>
+        <HomeHeroSection />
+        <HomeAboutSection />
+        <HomeTechnologiesSection />
+        <HomeProjectsSection />
+        <HomeContactSection />
+    </main>
 </template>
-<script></script>
